61da4b758659f8d83e0b977ff4013552a345879a
Xindi Xu <xindixu@utexas.edu>
2021-01-11 23:06:52 -0600

    feat(koa): implement router with two maps

--- koa/index.js
+++ koa/index.js
@@ -1,5 +1,7 @@
 const Koa = require("./src/index");
+const Router = require("./src/router");
 
+const router = new Router();
 const app = new Koa();
 
 // app.use(async (ctx, next) => {
@@ -11,19 +13,35 @@ const app = new Koa();
 
 const delay = () => new Promise((resolve) => setTimeout(() => resolve(), 2000));
 
-app.use(async (ctx, next) => {
-  ctx.body = "1";
-  await next();
-  ctx.body += "5";
+// app.use(async (ctx, next) => {
+//   ctx.body = "1";
+//   await next();
+//   ctx.body += "5";
+// });
+// app.use(async (ctx, next) => {
+//   ctx.body += "2";
+//   await delay();
+//   await next();
+//   ctx.body += "4";
+// });
+// app.use(async (ctx, next) => {
+//   ctx.body += "3";
+//   await next();
+// });
+
+router.get("/index", async (ctx) => {
+  ctx.body = "index page";
 });
-app.use(async (ctx, next) => {
-  ctx.body += "2";
-  await delay();
-  await next();
-  ctx.body += "4";
+router.get("/post", async (ctx) => {
+  ctx.body = "post page";
 });
-app.use(async (ctx, next) => {
-  ctx.body += "3";
+router.get("/list", async (ctx) => {
+  ctx.body = "list page";
 });
+router.post("/index", async (ctx) => {
+  ctx.body = "post page";
+});
+
+app.use(router.routes());
 
 app.listen(3000);
---
+++ koa/src/router.js
@@ -0,0 +1,38 @@
+class Router {
+  constructor() {
+    this.getMap = new Map();
+    this.postMap = new Map();
+  }
+
+  register(map, path, handler) {
+    map.set(path, handler);
+  }
+
+  get(path, handler) {
+    this.register(this.getMap, path, handler);
+  }
+
+  post(path, handler) {
+    this.register(this.postMap, path, handler);
+  }
+
+  routes() {
+    return async (ctx, next) => {
+      const path = ctx.url;
+      const { method } = ctx;
+      console.log(method);
+      const map = method === "get" ? this.getMap : this.postMap;
+
+      const fn = map.get(path);
+      if (fn) {
+        await fn(ctx);
+      }
+
+      console.log(fn);
+
+      await next();
+    };
+  }
+}
+
+module.exports = Router;
